6ae26776566a5de9fd2e37806670969a4c56ef1b
Sridhar Belagod <sriddbs.bs@gmail.com>
2025-10-19 14:57:11 +0530

    Implement search for embeddings

--- app/models/knowledge_entry.rb
+++ app/models/knowledge_entry.rb
@@ -2,4 +2,9 @@ class KnowledgeEntry < ApplicationRecord
   belongs_to :user
 
   has_neighbors :embedding
+
+  def self.search(query_embedding, limit: 10)
+    nearest_neighbors(:embedding, query_embedding, distance: "cosine")
+      .limit(limit)
+  end
 end
