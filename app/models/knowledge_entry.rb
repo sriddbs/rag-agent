0a3e291d9edd08954c39cbd1bc7ff12f99096fd0
Sridhar Belagod <sriddbs.bs@gmail.com>
2025-10-20 12:37:26 +0530

    Fix embedding search query

--- app/models/knowledge_entry.rb
+++ app/models/knowledge_entry.rb
@@ -4,7 +4,13 @@ class KnowledgeEntry < ApplicationRecord
   has_neighbors :embedding
 
   def self.search(query_embedding, limit: 10)
-    nearest_neighbors(:embedding, query_embedding, distance: "cosine")
-      .limit(limit)
+    vector = query_embedding.join(",")
+
+    find_by_sql([<<~SQL, limit])
+      SELECT *, (embedding <=> '[#{vector}]') AS distance
+      FROM #{table_name}
+      ORDER BY embedding <=> '[#{vector}]'
+      LIMIT ?
+    SQL
   end
 end
